94af194adff6dee4d48498eb3f5a8667529b45b8
Zach Stone <zachary.e.stone@gmail.com>
2013-11-20 14:02:08 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8>"
+	<title>Zach</title>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+</head>
+
+<body>
+<?php
+if(have_posts()) : while(have_posts()) : the_post();
+?>
+<h1> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h1>
+<?php the_content(); ?>
+<?php endwhile; else: ?>
+
+<?php endif; ?>
